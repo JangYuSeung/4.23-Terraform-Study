1c0af0989dcf8a230de978048c265bd8c5a67546
Jang Yuseung <jus0919@naver.com>
2026-04-27 20:17:47 +0900

    4.27 기록

--- 4_ALB_ASG_실습/4_2_alb/5_instance.tf
+++ 4_ALB_ASG_실습/4_2_alb/5_instance.tf
@@ -1,3 +1,6 @@
+# 인스턴스 생성 및 보안 그룹 규칙 추가 및 AMI 생성
+
+# 보안 그룹 규칙 추가: ALB에서 인스턴스로의 HTTP 트래픽 허용
 resource "aws_security_group_rule" "st8_ex_http_SG" {
     # 8080~8086 포트 범위에서 TCP 프로토콜로 인바운드 트래픽 허용
     type = "ingress"
@@ -50,7 +53,23 @@ resource "aws_key_pair" "st8_terraform_test_key" {
 }
 
 # ########################################################################
-# AMI 인스턴스 생성
+# <AMI 인스턴스 생성>
+# 참고: terraform apply로 현재 인스턴스로 이미지(AMI) 한번 생성한 이후에, 
+# 인스턴스 내용 수정해서 다시 apply 하면, 이미지(AMI)도 재생성될까?
+# => source_instance_id로 인스턴스를 참조하기 때문에, 인스턴스가 교체(replace)되느냐 아니냐에 따라 달라짐
+#
+# 1. AMI 재생성: 인스턴스가 destroy -> create되면 instance_id가 바뀌어 AMI 재생성됨
+# (참고): 아래가 바뀌면 인스턴스 교체 발생
+# # ami            = "ami-xxxxxxx"   # AMI 변경
+# instance_type  = "t3.small"      # 인스턴스 타입 변경
+# subnet_id      = ...             # 서브넷 변경
+# user_data      = ...             # user_data 변경 (user_data_replace_on_change = true 이므로)
+#
+# 2. AMI 재생성 X: 인스턴스 업데이트
+# (참고): 아래가 바뀌어도 인스턴스 교체가 아닌 업데이트 발생
+# tags                   = { Name = "new-name" }   # 태그 변경
+# vpc_security_group_ids = [...]                   # 보안그룹 변경
+# key_name               = "new-key"              # 키페어 변경
 # ========================================================================
 resource "aws_ami_from_instance" "st8_ex_ami" {
     name = "st8-ex-ami"
